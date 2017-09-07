cc4415037096556c3ef9d3dd773e068ce650b477
xorium <nicrontir@yandex.ru>
2017-09-07 10:14:41 +0300

    Update redshift.py

--- redshift.py
+++ redshift.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import datetime, os, argparse, time
+import datetime, os, argparse, time, getpass, sys
 
 start_hour = 19
 start_minute = 1
@@ -11,13 +11,17 @@ high_temp = 6500
 low_temp = 4000
 
 red_mode = False
-dbg = True
+dbg = False
 
 def shift_to_red():
+    global red_mode
+    red_mode = True
     if dbg: print("Shiffting into RED mode")
     os.popen("/usr/bin/redshift -O " + str(low_temp))
 
 def shift_to_blue():
+    global red_mode
+    red_mode = False
     if dbg: print("Shiffting into BLUE mode")
     os.popen("/usr/bin/redshift -O " + str(high_temp))
 
@@ -29,6 +33,10 @@ if __name__ == '__main__':
     parser.add_argument("--htemp", help="High temperature (6500 by default)")
     parser.add_argument("--ltemp", help="Low temperature (4000 by default)")
     args = parser.parse_args()
+
+    if getpass.getuser() != "root":
+        print("Script must be run by root user.")
+        #sys.exit()
 
     if args.start:
         start_hour = int(args.start.split(":")[0])
@@ -52,25 +60,27 @@ if __name__ == '__main__':
     wait_flag = True
     red_mode = False
     
+    #end_hour = 0
+    #end_minute = 28
+    
     while True:
         now = datetime.datetime.now()
         if end_hour < start_hour:
-            if now.hour >= start_hour and now.minute >= start_minute:
+            if now.hour >= start_hour and now.minute >= start_minute and not red_mode:
                 shift_to_red()
-            if now.hour >= 0 and now.minute >= 0 and now.hour <= end_hour and now.minute < end_minute:
+            if wait_flag and now.hour >= 0 and now.minute >= 0 and now.hour * 60 + now.minute < end_hour * 60 + end_minute:
                 wait_flag = False
-            if not wait_flag and now.hour >= end_hour and now.minute >= end_minute:
+                if not red_mode:
+                    shift_to_red()
+            if not wait_flag and now.hour >= end_hour and now.minute >= end_minute and red_mode:
                 shift_to_blue()
                 wait_flag = True
         else:
             if now.hour == start_hour:
                 if now.minute >= start_minute and not red_mode:
                     shift_to_red()
-                    red_mode = True
             elif now.hour > start_hour and now.hour < end_hour and not red_mode:
                 shift_to_red()
-                red_mode = True
             elif now.hour >= end_hour and now.minute >= end_minute and red_mode:
                 shift_to_blue()
-                red_mode = False
         time.sleep(timeout)
